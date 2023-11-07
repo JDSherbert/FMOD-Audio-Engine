b70d25c1d2e2873e653cdc6adc8c4f697a1f4211
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-11-07 14:08:15 +0000

    Update Utils.cpp

--- AudioEngine/Tools/Utils.cpp
+++ AudioEngine/Tools/Utils.cpp
@@ -1,1 +1,11 @@
+#include "Utils.h"
 
+float Utils::dBToVolume(float dB)
+{
+    return powf(10.0f, 0.05f * dB);
+}
+
+float Utils::VolumeTodB(float volume)
+{
+    return 20.0f * log10f(volume);
+}

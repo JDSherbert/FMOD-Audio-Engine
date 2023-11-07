15dbd143783cf6f88a1345caddf7103ee49ed3a1
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-11-07 14:07:47 +0000

    Update Utils.h

--- AudioEngine/Tools/Utils.h
+++ AudioEngine/Tools/Utils.h
@@ -1,1 +1,11 @@
+// ©2023 JDSherbert. All rights reserved.
 
+#pragma once
+
+class Utils
+{
+public:
+    /** Play the specified Audio File */
+    static float dBToVolume(float dB);
+    static float VolumeTodB(float volume);
+};

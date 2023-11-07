e7601ad59a75df48c0577ce2d620f04efbfc4519
JDSherbert <JDSherbert@outlook.com>
2023-11-07 17:02:09 +0000

    Add AudioData class

---
+++ AudioEngine/Source/Data/AudioData.cpp
@@ -0,0 +1,11 @@
+// ©2023 JDSherbert. All rights reserved.
+
+#include "AudioData.h"
+
+AudioData::AudioData()
+{
+}
+
+AudioData::~AudioData()
+{
+}
---
+++ AudioEngine/Source/Data/AudioData.h
@@ -0,0 +1,39 @@
+// ©2023 JDSherbert. All rights reserved.
+
+#pragma once
+
+#include "../Math/Vector3.h"
+
+class AudioData
+{
+private:
+
+    std::string uniqueID;
+    const char* filePath;
+    float volume;
+    bool loaded;
+    bool loop;
+    bool is3D;
+    unsigned int lengthMS;
+    float reverbAmount;
+    Vector3 position;
+    
+public:
+
+    AudioData();
+    ~AudioData();
+
+    std::string GetUniqueID() const { return uniqueID; };
+    const char* GetFilePath() const { return filePath; }
+    float GetVolume() const { return volume; }
+    bool IsLoaded() const { return loaded; };
+    bool Loop() const { return loop; };
+    bool Is3D() const { return is3D; };
+    float GetReverbAmount() const { return reverbAmount; }
+    Vector3 GetPosition() const { return position; }
+
+    void SetLoaded(bool isLoaded) { loaded = isLoaded; }
+    void SetLengthMS(unsigned int length) { lengthMS = length; }
+    void SetVolume(float newVolume) { volume = newVolume; }
+
+};
